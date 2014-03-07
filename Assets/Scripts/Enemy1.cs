d19035af0870007bded0513c6c8e6fc506e3b8e6
Avraam Mavridis <avraammauridis@gmail.com>
2014-03-07 22:57:11 +0100

    Great Escape first commit

---
+++ Assets/Scripts/Enemy1.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy1 : MonoBehaviour {
+
+    private Vector2 screen_end_coordinates;
+    private Vector2 screen_start_coordinates;
+    int direction;
+    Animator anim;
+
+	// Use this for initialization
+	void Start () {
+        screen_end_coordinates = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
+        screen_start_coordinates = Camera.main.ScreenToWorldPoint(new Vector2(0, 0));
+        direction = 0;
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        MoveEnemy();
+	}
+
+    void MoveEnemy()
+    {
+       if (rigidbody2D.transform.position.x > screen_start_coordinates.x + 5 && direction==0) { 
+            rigidbody2D.transform.position = new Vector2(rigidbody2D.transform.position.x - 0.15f, rigidbody2D.transform.position.y);
+       }
+       else if (rigidbody2D.transform.position.x < screen_start_coordinates.x + 5 && direction == 0)
+       {
+           direction = 1;
+           anim.SetInteger("direction", 1);
+           
+       }
+       else if (rigidbody2D.transform.position.x < screen_end_coordinates.x + 5 && direction == 1)
+       {
+           rigidbody2D.transform.position = new Vector2(rigidbody2D.transform.position.x + 0.15f, rigidbody2D.transform.position.y);
+       }
+       else if (rigidbody2D.transform.position.x > screen_end_coordinates.x + 5 && direction == 1)
+       {
+           direction = 0;
+           anim.SetInteger("direction", 0);
+       }
+
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+    private Vector2 player_position;
+    float speed = 500;
+    Animator anim;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        MovePlayer();
+	}
+
+    enum Direction { down, up }
+    Direction dir;
+
+    void MovePlayer()
+    {
+        player_position = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        rigidbody2D.AddForce(player_position*speed);
+        
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+           anim.SetInteger("direction", 1);
+           dir=Direction.up;
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            anim.SetInteger("direction", 0);
+            dir=Direction.down;
+        }
+        else if (Input.anyKey == false)
+        {
+            if(dir == Direction.up)
+                anim.SetInteger("direction", 3);
+            else if(dir == Direction.down)
+                anim.SetInteger("direction", 2);
+        }
+        
+       
+    }
+}
